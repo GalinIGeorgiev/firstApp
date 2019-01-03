c6400b0bc4467a57a4c6223f3e6eda1c1f7c610d
GalinIGeorgiev <galin.georgiev.ggg@gmail.com>
2019-01-03 19:20:07 +0200

    Introduced SandBox for testing

---
+++ src/Tests/SandBox/Program.cs
@@ -0,0 +1,51 @@
+﻿using CommandLine;
+using FirstApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+           
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+
+                SandBoxCode(serviceProvider); 
+            }
+        }
+
+        private static void SandBoxCode(IServiceProvider serviceProvider)
+        {
+              
+        }
+
+
+        private static void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            serviceCollection.AddDbContext<FirstAppContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("FirstAppContextConnection")));
+        }
+    }
+}
