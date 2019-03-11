4e4d205c1c9fb48e6d696ca20c89b8bfa5e225d5
GalinIGeorgiev <galin.georgiev.ggg@gmail.com>
2019-03-11 16:45:44 +0200

    Add Article Controller,modify layout

--- src/Web/Areas/Administration/Controllers/ArticlesController.cs
+++ src/Web/Areas/Administration/Controllers/ArticlesController.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstApp.Web.Areas.Administration.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ArticlesController : AdministrationBaseController
     {
 
+        public ArticlesController()
+        {
+
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //[HttpPost]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
     }
 }
