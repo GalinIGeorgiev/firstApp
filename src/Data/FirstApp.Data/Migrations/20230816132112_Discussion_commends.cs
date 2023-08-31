b98a34de1eecbecfef88130cd6faf1aa3b3d3635
GalinIGeorgiev <galin.georgiev.ggg@gmail.com>
2023-08-31 19:14:08 +0300

    Discussion Migrations

---
+++ src/Data/FirstApp.Data/Migrations/20230816132112_Discussion_commends.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstApp.Data.Migrations
+{
+    public partial class Discussion_commends : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
---
+++ src/Data/FirstApp.Data/Migrations/20230817090100_AddActivityToDisscusion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstApp.Data.Migrations
+{
+    public partial class AddActivityToDisscusion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastActivity",
+                table: "Discussions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastActivity",
+                table: "Discussions");
+        }
+    }
+}
